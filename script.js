987364c402e9a108983851535fcaf65311512673
PARTH-kodes <officialparth18@gmail.com>
2024-06-22 05:54:57 +0530

    optimization commit

--- script.js
+++ script.js
@@ -1,81 +1,78 @@
 function game(yourChoice) {
-    var player, computer;
+  const player = yourChoice.id;
+  console.log("Your Choice: ", player);
 
-    player = yourChoice.id;
-    console.log('Your Choice: ', player)
+  const computer = computerChoice(generateRandomNumber());
+  console.log("Computer choice: ", computer);
 
-    computer = computerChoice(generateRandomNumber());
-    console.log('Computer choice: ', computer);
+  const results = decideWinner(player, computer);
+  console.log(results);
 
-    results = decideWinner(player, computer);
-    console.log(results);
+  const message = findMessage(results);
+  console.log(message);
 
-    message = findMessage(results);
-    console.log(message);
-
-    frontEnd(player, computer, message);
+  frontEnd(player, computer, message);
 }
 
-//generate random number from 0,1,2
+// Generate a random number from 0, 1, 2
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 3);
+  return Math.floor(Math.random() * 3);
 }
 
-//choose for computer
+// Choose for computer
 function computerChoice(number) {
-    return ['rock', 'paper', 'scissor'][number];
+  return ["rock", "paper", "scissor"][number];
 }
 
-//cases for winning game
+// Determine the winner
 function decideWinner(yourChoice, computerChoice) {
-    var rpsDatabase = {
-        'rock': { 'rock': 0.5, 'paper': 0, 'scissor': 1 },
-        'paper': { 'rock': 1, 'paper': 0.5, 'scissor': 0 },
-        'scissor': { 'rock': 0, 'paper': 1, 'scissor': 0.5 }
-    }
-    //outpus is in format [0,1]
-    var yourScore = rpsDatabase[yourChoice][computerChoice];
-    var computerScore = rpsDatabase[computerChoice][yourChoice];
+  const rpsDatabase = {
+    rock: { rock: 0.5, paper: 0, scissor: 1 },
+    paper: { rock: 1, paper: 0.5, scissor: 0 },
+    scissor: { rock: 0, paper: 1, scissor: 0.5 },
+  };
 
-    return [yourScore, computerScore];
+  const yourScore = rpsDatabase[yourChoice][computerChoice];
+  const computerScore = rpsDatabase[computerChoice][yourChoice];
+
+  return [yourScore, computerScore];
 }
 
-//Return an object for printing the result with particular color
+// Return a message object with the result and color
 function findMessage([yourScore, computerScore]) {
-    if (yourScore === 1) {
-        return { 'text': 'You Win', 'color': '#40C4FF' };
-    }
-    else if (yourScore === 0) {
-        return { 'text': 'You Lose', 'color': '#FF5252' };
-    }
-    else {
-        return { 'text': 'Draw', 'color': 'white' };
-    }
+  if (yourScore === 1) {
+    return { text: "You Win", color: "#40C4FF" };
+  } else if (yourScore === 0) {
+    return { text: "You Lose", color: "#FF5252" };
+  } else {
+    return { text: "Draw", color: "white" };
+  }
 }
 
-//changes the frontend after choosing the hand
+// Update the front end after a choice is made
 function frontEnd(player, computer, message) {
-    var imageDatabase = {
-        'rock': document.getElementById('rock').src,
-        'paper': document.getElementById('paper').src,
-        'scissor': document.getElementById('scissor').src
-    };
+  const imageDatabase = {
+    rock: document.getElementById("rock").src,
+    paper: document.getElementById("paper").src,
+    scissor: document.getElementById("scissor").src,
+  };
 
-    //remove all the images
-    document.getElementById('rock').remove();
-    document.getElementById('paper').remove();
-    document.getElementById('scissor').remove();
+  const rpsDiv = document.getElementById("rps-div");
+  rpsDiv.innerHTML = ""; // Clear previous images
 
-    //create the items to be displayed after choosing hand
-    var humandiv = document.createElement('div');
-    var computerdiv = document.createElement('div');
-    var messagediv = document.createElement('div');
+  const elements = [
+    { type: "img", src: imageDatabase[player], boxShadow: "#40C4FF" },
+    { type: "h1", text: message["text"], color: message["color"] },
+    { type: "img", src: imageDatabase[computer], boxShadow: "#FF5252" },
+  ];
 
-    humandiv.innerHTML = "<img src='" + imageDatabase[player] + "' height=150 width=auto style='box-shadow: 3px 3px 10px 10px #40C4FF'>";
-    messagediv.innerHTML = "<h1 style='color:" + message['color'] + "; font-size:50px; padding: 20px;'>" + message['text'] + "</h1>";
-    computerdiv.innerHTML = "<img src='" + imageDatabase[computer] + "'height=150 width=auto style='box-shadow: 3px 3px 10px 10px #FF5252'>";
-
-    document.getElementById('rps-div').appendChild(humandiv);
-    document.getElementById('rps-div').appendChild(messagediv);
-    document.getElementById('rps-div').appendChild(computerdiv);
+  elements.forEach((elem) => {
+    const div = document.createElement("div");
+    if (elem.type === "img") {
+      div.innerHTML = `<img src='${elem.src}' height=150 width=auto style='box-shadow: 3px 3px 10px 10px ${elem.boxShadow}'>`;
+    } else if (elem.type === "h1") {
+      div.innerHTML = `<h1 style='color:${elem.color}; font-size:50px; padding: 20px;'>${elem.text}</h1>`;
+    }
+    rpsDiv.appendChild(div);
+  });
 }
