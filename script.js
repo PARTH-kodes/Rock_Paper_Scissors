3f57a2328c0ce6137ea99c40121d09ded025cbbb
PARTH-kodes <officialparth18@gmail.com>
2024-06-22 10:33:40 +0530

    added Play Again

--- script.js
+++ script.js
@@ -41,38 +41,51 @@ function decideWinner(yourChoice, computerChoice) {
 // Return a message object with the result and color
 function findMessage([yourScore, computerScore]) {
   if (yourScore === 1) {
-    return { text: "You Win", color: "#40C4FF" };
+    return { text: "You Win", color: "green" };
   } else if (yourScore === 0) {
     return { text: "You Lose", color: "#FF5252" };
   } else {
-    return { text: "Draw", color: "white" };
+    return { text: "Draw", color: "blue" };
   }
 }
 
 // Update the front end after a choice is made
 function frontEnd(player, computer, message) {
   const imageDatabase = {
-    rock: document.getElementById("rock").src,
-    paper: document.getElementById("paper").src,
-    scissor: document.getElementById("scissor").src,
+    rock: "rock.png",
+    paper: "paper.png",
+    scissor: "scissor.png",
   };
 
   const rpsDiv = document.getElementById("rps-div");
   rpsDiv.innerHTML = ""; // Clear previous images
 
-  const elements = [
-    { type: "img", src: imageDatabase[player], boxShadow: "#40C4FF" },
-    { type: "h1", text: message["text"], color: message["color"] },
-    { type: "img", src: imageDatabase[computer], boxShadow: "#FF5252" },
-  ];
+  const playerDiv = document.createElement("div");
+  playerDiv.innerHTML = `<img src='${imageDatabase[player]}' height=150 width=auto style='box-shadow: 3px 3px 10px 10px #40C4FF;'>`;
+  rpsDiv.appendChild(playerDiv);
 
-  elements.forEach((elem) => {
-    const div = document.createElement("div");
-    if (elem.type === "img") {
-      div.innerHTML = `<img src='${elem.src}' height=150 width=auto style='box-shadow: 3px 3px 10px 10px ${elem.boxShadow}'>`;
-    } else if (elem.type === "h1") {
-      div.innerHTML = `<h1 style='color:${elem.color}; font-size:50px; padding: 20px;'>${elem.text}</h1>`;
-    }
-    rpsDiv.appendChild(div);
-  });
+  const messageDiv = document.createElement("div");
+  messageDiv.innerHTML = `<h1 id='result-message' style='color:${message.color}; font-size:28px; padding: 20px;'>${message.text}</h1>`;
+  rpsDiv.appendChild(messageDiv);
+
+  const computerDiv = document.createElement("div");
+  computerDiv.innerHTML = `<img src='${imageDatabase[computer]}' height=150 width=auto style='box-shadow: 3px 3px 10px 10px #FF5252;'>`;
+  rpsDiv.appendChild(computerDiv);
+
+  // Show the "Play Again" button
+  document.getElementById("play-again").style.display = "block";
+}
+
+// Reset the game
+function resetGame() {
+  // Hide the "Play Again" button
+  document.getElementById("play-again").style.display = "none";
+
+  // Reset the images
+  const rpsDiv = document.getElementById("rps-div");
+  rpsDiv.innerHTML = `
+    <img src="rock.png" alt="rock" id="rock" onclick="game(this)" />
+    <img src="paper.png" alt="paper" id="paper" onclick="game(this)" />
+    <img src="scissor.png" alt="scissor" id="scissor" onclick="game(this)" />
+  `;
 }
